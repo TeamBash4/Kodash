d230c5c4b0c8aec6fe9e84e1df6ea34e5049bf08
Partha <ytiamahtrap@gmail.com>
2022-06-09 10:26:03 +0530

    :sparkles: add click to upload

--- scripts/hosting.js
+++ scripts/hosting.js
@@ -21,10 +21,10 @@ var startButton = document.getElementById('start')
 
 window.addEventListener('load', genSiteId)
 
-function genSiteId(){
+function genSiteId() {
     console.log("I am in")
-siteId.value = randomString(7);
-checkSiteId();
+    siteId.value = randomString(7);
+    checkSiteId();
 }
 siteId.addEventListener('keyup', checkSiteId);
 
@@ -52,87 +52,85 @@ function checkSiteId() {
 
 // Fetching the site duration from user
 function getDuration() {
-    var getSelectedValue = document.querySelector( 'input[name="timeExpire"]:checked');   
-    if(getSelectedValue != null) {   
-             return getSelectedValue.value;  
+    var getSelectedValue = document.querySelector('input[name="timeExpire"]:checked');
+    if (getSelectedValue != null) {
+        return getSelectedValue.value;
+    } else {
+        return "6h";
     }
-     else {  
-                return "6h";
-     } 
 }
 let dropArea = document.getElementById('dragArea');
 
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+;
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
     dropArea.addEventListener(eventName, preventDefaults, false)
-  })
-  
-function preventDefaults (e) {
-e.preventDefault()
-e.stopPropagation()
+})
+
+function preventDefaults(e) {
+    e.preventDefault()
+    e.stopPropagation()
 }
 dropArea.addEventListener('drop', handleDrop, false)
 
 function handleDrop(e) {
     let dt = e.dataTransfer
     files = dt.files
-  }
+}
 
 document.getElementById("start").addEventListener('click', startHostNow)
-function startHostNow()
-{
+
+function startHostNow() {
     for (var i = 0; i < files.length; i++) {
-        if (files[i].name==="index.html") {
+        if (files[i].name === "index.html") {
             flag = 1;
         }
     }
     if (flag == 1) {
-    startButton.innerText = "Hosting...";
-    handleFiles(files)
-    var hostedSites = localStorage.getItem("sites");
-    if(hostedSites == null)
-    {
-        console.log("abhi khaali")
-        localStorage.setItem("sites", [siteId.value]);
+        startButton.innerText = "Hosting...";
+        handleFiles(files)
+        var hostedSites = localStorage.getItem("sites");
+        if (hostedSites == null) {
+            console.log("abhi khaali")
+            localStorage.setItem("sites", [siteId.value]);
+        } else {
+            console.log("abhi nahi khaali")
+            console.log(hostedSites)
+            hostedSites += "," + siteId.value;
+            localStorage.setItem("sites", hostedSites);
+        }
+    } else {
+        alert("Please upload index.html file");
     }
-    else
-    {
-        console.log("abhi nahi khaali")
-        console.log(hostedSites)
-        hostedSites += "," + siteId.value;
-        localStorage.setItem("sites", hostedSites);
-    }
-}
-else
-{
-    alert("Please upload index.html file");
-}
 
-function handleFiles(files) {
-    ([...files]).forEach(uploadFile)
-    
-  }
+    function handleFiles(files) {
+        ([...files]).forEach(uploadFile)
+
+    }
+}
 
 /* Sending Files to Kodash server */
 
-  function uploadFile(file) {
+function uploadFile(file) {
     let formData = new FormData()
     formData.append('file', file)
     var url = `${apiURL}/host?duration=${getDuration()}&path=${siteId.value}`
     fetch(url, {
-      method: 'POST',
-      body: formData
-    })
-    .then(() => { 
-        startButton.remove();
-        let hsButton = document.getElementById("hsButton");
-        hsButton.href = `${domain}/${siteId.value}`;
-        hsButton.style.backgroundColor = "#00bcd4";
-        hsButton.style.display = "block";
-        hsButton.target = "_blank";
-})
-    .catch(() => { console.log("Failed!") })
-    
-  }
+            method: 'POST',
+            body: formData
+        })
+        .then(() => {
+            startButton.remove();
+            let hsButton = document.getElementById("hsButton");
+            hsButton.href = `${domain}/${siteId.value}`;
+            hsButton.style.backgroundColor = "#00bcd4";
+            hsButton.style.display = "block";
+            hsButton.target = "_blank";
+        })
+        .catch(() => {
+            console.log("Failed!")
+        })
+
+}
 
 // ********************************************** //
 // Get Random String
@@ -148,3 +146,13 @@ function randomString(length) {
     }
     return result;
 }
+
+
+// ********************************************** //
+//                 Click to Upload                //
+// ********************************************** //
+
+const dragEl = document.getElementById('dragArea');
+const inputFileEl = document.getElementById('inputFile');
+
+dragEl.addEventListener('click', () => inputFileEl.click());
